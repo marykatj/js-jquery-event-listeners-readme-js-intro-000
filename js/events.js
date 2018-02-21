2bd48d46492b9ba0e5add7589b1cc3684803a8fb
Mary-Kate James <marykatj@gmail.com>
2018-02-21 21:42:17 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,51 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass('tasty');
+    $('img').style.tasty {
+      border-width: "5px";
+      border-color: red;
+      border-style: solid;
+    }
+  });
+}
+
+function pressIt() {
+  $("form").on("keydown", function() {
+    if ($("input").val() === 71) {
+      alert('G was pressed');
+      return;
+    }
+    return;
+  });
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  });
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+/*
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass('tasty');
+    tasty.style.borderColor = "#FF0000";
+  });
+}
+*/

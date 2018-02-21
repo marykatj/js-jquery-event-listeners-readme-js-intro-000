afc3c8d828a1dc4a84a83edd779598baa726c7f5
Mary-Kate James <marykatj@gmail.com>
2018-02-21 22:30:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt() {
 function pressIt() {
   $('form').on("keydown", function() {
     if ($('input').val() === 71) {
-      alert('G was pressed');
+      alert('G was pressed.');
     }
   });
 }

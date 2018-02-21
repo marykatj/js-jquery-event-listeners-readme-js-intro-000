98bc31ea00d34d634daa213af22233e38f4993ae
Mary-Kate James <marykatj@gmail.com>
2018-02-21 22:52:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,49 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('#typing').on("keydown", function(e) {
+    if (e.which === 71) {
+      alert('G was pressed');
+    }
+  });
+}
+
+//
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  });
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+/*
+
+function pressIt() {
+  $("form").on("keydown", function() {
+    if ($("input").val() === 71) {
+      alert('G was pressed');
+      return;
+    }
+    return;
+  });
+}
+*/
